d0b195ec2091f8710df9749d3cf7f7d8869ad368
sujan7989 <sujan7989@gmail.com>
2026-04-26 11:44:47 +0530

    feat: add render.yaml, vercel.json, deployment configs, env files

--- playto_kyc/settings.py
+++ playto_kyc/settings.py
@@ -12,6 +12,9 @@ DEBUG = os.getenv('DEBUG', 'True') == 'True'
 
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost,127.0.0.1,0.0.0.0').split(',')
 
+# Trust Render's proxy
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
